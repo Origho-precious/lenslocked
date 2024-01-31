bb151a8a2785bd7cbda51d9eb3fa1a87e8e72ce9
Origho-precious <Origho9@gmail.com>
2024-01-31 23:36:35 +0100

    feat: adding and using a template for home route

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,7 +13,30 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+
+	path := filepath.Join("templates", "home.gohtml")
+
+	tpl, err := template.ParseFiles(path)
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(
+			w,
+			"Something went wrong while parsing the template",
+			http.StatusInternalServerError,
+		)
+
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(
+			w,
+			"Something went wrong while executing the template",
+			http.StatusInternalServerError,
+		)
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
