242b9054cd4533c0476dd2f0aefe8941bc5a0ac1
Origho-precious <Origho9@gmail.com>
2024-02-03 18:20:54 +0100

    feat: adding template.go file for handling HTML templates

---
+++ views/template.go
@@ -0,0 +1,39 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func Parse(path string, data interface{}) (Template, error) {
+	footerPath := filepath.Join("templates", "footer.gohtml")
+
+	tpl, err := template.ParseFiles(path, footerPath)
+
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing tempate: %w", err)
+	}
+
+	return Template{htmlTpl: tpl}, nil
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	err := t.htmlTpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(
+			w,
+			"Something went wrong while executing the template",
+			http.StatusInternalServerError,
+		)
+	}
+}
