0a84bdd6cac1070a392e15f0f80c3d366d893fbe
Origho-precious <Origho9@gmail.com>
2024-02-04 17:25:47 +0100

    feat: adding controller func for FAQ

--- controllers/static.go
+++ controllers/static.go
@@ -2,11 +2,39 @@ package controllers
 
 import (
 	"github/Origho-precious/lenslocked/views"
+	"html/template"
 	"net/http"
 )
 
-func Statichandler(tpl views.Template, data interface{}) http.HandlerFunc {
+type FaqStruct struct {
+	Question string
+	Answer   template.HTML
+}
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tpl.Execute(w, data)
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []FaqStruct{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plan.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have a support staff answering 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer: `Email us - 
+			<a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
