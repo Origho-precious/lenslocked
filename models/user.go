022e6ced21a1ea472afcf1135b311222c04013ae
Origho-precious <Origho9@gmail.com>
2024-02-18 10:31:55 +0100

    chore: adding Create method on UserService

--- models/user.go
+++ models/user.go
@@ -1,6 +1,12 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	Id           uint
@@ -10,4 +16,37 @@ type User struct {
 
 type UserService struct {
 	DB *sql.DB
+}
+
+func (us *UserService) Create(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+
+	hashByte, err := bcrypt.GenerateFromPassword(
+		[]byte(password), bcrypt.DefaultCost,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("creating new user: %w", err)
+	}
+
+	passwordHash := string(hashByte)
+
+	row := us.DB.QueryRow(`
+		INSERT INTO users (email, password_hash)
+		VALUES ($1, $2) RETURNING id
+	`, email, passwordHash,
+	)
+
+	newUser := User{
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+
+	err = row.Scan(&newUser.Id)
+
+	if err != nil {
+		return nil, fmt.Errorf("creating new user: %w", err)
+	}
+
+	return &newUser, nil
 }
